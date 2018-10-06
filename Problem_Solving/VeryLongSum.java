483f407c7b259e270b298ca9300e99b66cf3434f
unaidelao <unai.delao@gmail.com>
2018-10-06 02:26:17 +0200

    Add VeryLongSum.java

---
+++ Problem_Solving/VeryLongSum.java
@@ -0,0 +1,54 @@
+/**
+ * A very long sum - Java
+ * 
+ * Calculate and print the sum of the elements in an array, keeping in mind that some of those
+ * integers may be quite large.
+ * 
+ * You might need to use long long int in C/C++ or long data type in Java to store such sums.
+ * 
+ * @author Unai de la O
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+public class Solution {
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+        return LongStream.of(ar).sum();
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
