36331741640ac1531d9a8cae2bae9951fa1e288b
unaidelao <unai.delao@gmail.com>
2018-10-06 02:27:22 +0200

    moved file: WelcomeToJava.java

---
+++ Java_Proficiency/WelcomeToJava.java
@@ -0,0 +1,13 @@
+/**
+ * Welcome to Java - Java
+ * 
+ * @author Unai de la O
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
