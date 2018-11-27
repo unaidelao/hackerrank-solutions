5f9cd5af3df9dfb8fde07216c7ed806a3c65bb31
unaidelao <unai.delao@gmail.com>
2018-11-27 10:10:52 +0100

    Add loops.py

---
+++ Python_Proficiency/Introduction/loops.py
@@ -0,0 +1,21 @@
+"""
+==Task==
+Read an integer N. For all non-negative integers i < N, print i^2. See the sample for details.
+
+==Input Format==
+
+The first and only line contains the integer, N.
+
+==Constraints==
+i <= N <= 20
+
+==Output Format==
+
+Print N lines, one corresponding to each i.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(0, n):
+    print(i*i)
