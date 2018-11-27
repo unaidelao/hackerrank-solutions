d9d468623b55b117b9abe956c295764f02c65d94
unaidelao <unai.delao@gmail.com>
2018-11-27 10:11:52 +0100

    Add print_function.py

---
+++ Python_Proficiency/Introduction/print_function.py
@@ -0,0 +1,22 @@
+"""
+==Task==
+Read an integer N.
+
+Without using any string methods, try to print the following:
+
+123···N
+
+Note that "···" represents the values in between.
+
+==Input Format==
+The first line contains an integer N.
+
+==Output Format==
+Output the answer as explained in the task.
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+
+for i in range(0, n):
+    print(i+1, end="")
