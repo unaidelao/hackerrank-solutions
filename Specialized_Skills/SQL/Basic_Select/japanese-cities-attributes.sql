12f49638ec9ad768a46f0d986e3a0b4d2c57ea41
unaidelao <unai.delao@gmail.com>
2019-01-09 00:44:27 +0100

    Add japanese-cities-attributes.sql

---
+++ Specialized_Skills/SQL/Basic_Select/japanese-cities-attributes.sql
@@ -0,0 +1,8 @@
+/*
+Japanese Cities' Attributes
+(ORACLE - MySQL - MS SQL SERVER)
+
+== Task ==
+Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+*/
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
